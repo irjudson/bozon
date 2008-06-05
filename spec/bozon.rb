17ec362e33a9fb874c193036aa2b332b8acdea5a
Ivan R. Judson <irjudson@gmail.com>
2008-06-05 11:42:08 -0600

    added incomplete test

---
+++ spec/bozon.rb
@@ -0,0 +1,9 @@
+require File.dirname(__FILE__) + 'spec_helper'
+
+describe Bozon do
+  before(:each) do
+    # Required data members
+    # version
+    # type [Basic Data Definition, Model Component, Actual Piece of Data]
+    # stuff 
+    @json = {}
