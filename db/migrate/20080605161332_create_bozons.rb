3597edbd64c5deaced59e88569340a36b0a7e25e
Ivan R. Judson <ivan.judson@montana.edu>
2008-09-26 16:42:52 -0600

    I have no idea what i changed.

--- db/migrate/20080605161332_create_bozons.rb
+++ db/migrate/20080605161332_create_bozons.rb
@@ -2,6 +2,8 @@ class CreateBozons < ActiveRecord::Migration
   def self.up
     create_table :bozons do |t|
       t.text :content
+      t.text :content_id
+      t.text :parent_id
       t.timestamps
     end
   end
